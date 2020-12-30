53e11273c0c6cd0431fc59525c31dd0dbd7e41bb
screamer <jhu@nwpu.edu.cn>
2020-12-30 20:48:32 +0800

    simple code in test

--- tests/vipcca_test.py
+++ tests/vipcca_test.py
@@ -1,14 +1,17 @@
-import VIPCCA
-import VIPCCA.preprocessing as pp
-import VIPCCA.plotting as pl
+import VIPCCA as vp
+from VIPCCA import preprocessing as pp
+from VIPCCA import plotting as pl
 
+# read single-cell data.
 adata_b1 = pp.read_sc_data("./data/mixed_cell_lines/293t.h5ad", batch_name="293t")
 adata_b2 = pp.read_sc_data("./data/mixed_cell_lines/jurkat.h5ad", batch_name="jurkat")
 adata_b3 = pp.read_sc_data("./data/mixed_cell_lines/mixed.h5ad", batch_name="mixed")
 
 # pp.preprocessing include filteration, log-TPM normalization, selection of highly variable genes.
 adata_all= pp.preprocessing([adata_b1, adata_b2, adata_b3])
-handle = VIPCCA.VIPCCA(
+
+# VIPCCA will train the neural network on the provided datasets.
+handle = vp.VIPCCA(
 							adata_all,
 							res_path='./results/CVAE_5/',
 							split_by="_batch",
@@ -18,9 +21,11 @@ handle = VIPCCA.VIPCCA(
 							# uncomment the following line if a pretrained model was provided in the result folder.
 							# model_file="model.h5" 
 							)
+
+# transform user's single-cell data into shared low-dimensional space and recover gene expression.
 adata_transform=handle.fit_transform()
 
-pl.plotPrediction2(adata_transform.raw.X,adata_transform.X,result_path=test_result_path,rnum=2000,lim=20)
+# Visualization
 pl.run_embedding(adata_transform, path=test_result_path,method="umap")
 pl.plotEmbedding(adata_transform, path=test_result_path, method='umap', group_by="_batch",legend_loc="right margin")
 pl.plotEmbedding(adata_transform, path=test_result_path, method='umap', group_by="celltype",legend_loc="on data")
