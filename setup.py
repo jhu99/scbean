f76e37f70ff50082fd690afe5316d448278f2ac5
drizzlezyk <43944770+drizzlezyk@users.noreply.github.com>
2021-01-01 19:15:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 install_requires = [
-      'scanpy',
-      'tensorflow',
+      'scanpy==1.6.0',
+      'tensorflow==1.13.1',
       'anndata',
       'scipy',
       'pandas',
       'seaborn',
-      'keras',
+      'keras==2.3',
       'python-igraph',
       'louvain'
 ]
@@ -20,6 +20,7 @@ setup(name='VIPCCA',
       version='0.1.5',
       description='VIPCCA',
       long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/JHuLab/VIPCCA',
       author='Jialu Hu',
       author_email='jialuhu@umich.edu',
@@ -27,4 +28,4 @@ setup(name='VIPCCA',
       packages=find_packages(),
       install_requires=install_requires,
       zip_safe=False,
-      python_requires='>=3.6',)
+      python_requires='==3.6',)
