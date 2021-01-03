724e2922311f5417a12dce4e003ea14d182847ac
drizzlezyk <43944770+drizzlezyk@users.noreply.github.com>
2021-01-03 20:30:26 +0800

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./vipcca'))
+# sys.path.insert(0, os.path.abspath('./vipcca'))
 
-# root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-# sys.path.insert(0, root_dir)
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.insert(0, root_dir)
 
 # -- Project information -----------------------------------------------------
 
@@ -32,7 +32,17 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx.ext.todo', 'sphinx.ext.viewcode',  'sphinx.ext.autodoc']
+extensions = [
+'recommonmark',
+'sphinx.ext.todo', 
+'sphinx.ext.viewcode',  
+'sphinx.ext.autodoc',
+'sphinx.ext.autosummary',
+'sphinx.ext.napoleon',
+'sphinx.ext.coverage',
+'sphinx_autodoc_typehints',
+'sphinx.ext.mathjax',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
