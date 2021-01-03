78d5dce83104f8426ccd1dea139557a6ab2373ec
drizzlezyk <43944770+drizzlezyk@users.noreply.github.com>
2021-01-03 15:29:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 install_requires = [
       'scanpy==1.6.0',
-      'tensorflow==1.15.2',
+      'tensorflow==1.15.4',
       'anndata',
       'scipy',
       'pandas',
