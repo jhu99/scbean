db75fe3b7db4ff6d0bd441dc1025041dd34e8eb3
Jialu <jhu@nwpu.edu.cn>
2021-01-18 22:10:00 +0800

    Update vipcca_test.py

--- tests/vipcca_test.py
+++ tests/vipcca_test.py
@@ -2,19 +2,19 @@ import scbean.model.vipcca as vip
 import scbean.tools.utils as tl
 import scbean.tools.plotting as pl
 
-# If your script depends on a specific backend you can use the use() function:
+# Please choose an appropiate matplotlib backend.
 import matplotlib
-matplotlib.use('TkAgg')
+# matplotlib.use('TkAgg')
 
 # read single-cell data.
 adata_b1 = tl.read_sc_data("./data/mixed_cell_lines/293t.h5ad", batch_name="293t")
 adata_b2 = tl.read_sc_data("./data/mixed_cell_lines/jurkat.h5ad", batch_name="jurkat")
 adata_b3 = tl.read_sc_data("./data/mixed_cell_lines/mixed.h5ad", batch_name="mixed")
 
-# pp.preprocessing include filteration, log-TPM normalization, selection of highly variable genes.
+# tl.preprocessing include filteration, log-TPM normalization, selection of highly variable genes.
 adata_all= tl.preprocessing([adata_b1, adata_b2, adata_b3])
 
-# VIPCCA will train the neural network on the provided datasets.
+# Construct VIPCCA with specific setting.
 handle = vip.VIPCCA(
 							adata_all,
 							res_path='./results/CVAE_5/',
@@ -23,10 +23,10 @@ handle = vip.VIPCCA(
 							lambda_regulizer=5,
 							)
 
-# transform user's single-cell data into shared low-dimensional space and recover gene expression.
-adata_transform=handle.fit_integrate()
+# Training and integrating multiple single-cell datasets. The VIPCCA's output include cell representation in reduced dimensional space and recovered gene expression.
+adata_integrate=handle.fit_integrate()
 
 # Visualization
-pl.run_embedding(adata_transform, path='./results/CVAE_5/',method="umap")
-pl.plotEmbedding(adata_transform, path='./results/CVAE_5/', method='umap', group_by="_batch",legend_loc="right margin")
-pl.plotEmbedding(adata_transform, path='./results/CVAE_5/', method='umap', group_by="celltype",legend_loc="on data")
+pl.run_embedding(adata_integrate, path='./results/CVAE_5/',method="umap")
+pl.plotEmbedding(adata_integrate, path='./results/CVAE_5/', method='umap', group_by="_batch",legend_loc="right margin")
+pl.plotEmbedding(adata_integrate, path='./results/CVAE_5/', method='umap', group_by="celltype",legend_loc="on data")
