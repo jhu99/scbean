68475769f9b224cb5c348f0dee4c4c35c1d59cc6
drizzlezyk <43944770+drizzlezyk@users.noreply.github.com>
2021-04-04 10:12:37 +0800

    Update davae.py

--- scbean/model/davae.py
+++ scbean/model/davae.py
@@ -165,9 +165,10 @@ class DAVAE:
 
         dann_vae = Model([inputs_x, inputs_batch, inputs_loss_weights], [outputs_x, domain_pred], name='vae_mlp')
 
-        inputs_x_value = tf.multiply(inputs_x, inputs_loss_weights)
-        outputs_x_value = tf.multiply(outputs_x, inputs_loss_weights)
-        reconstruction_loss = mse(inputs_x_value, outputs_x_value)
+        inputs_x = tf.multiply(inputs_x, inputs_loss_weights)
+        outputs_x = tf.multiply(outputs_x, inputs_loss_weights)
+
+        reconstruction_loss = mse(inputs_x, outputs_x)
         # reconstruction_loss = mse(inputs_x, outputs_x)
 
         noise = tf.math.subtract(inputs_x, outputs_x)
@@ -291,11 +292,11 @@ class DACVAE:
 
         dann_vae = Model([inputs_x, inputs_batch, inputs_loss_weights], [outputs_x, domain_pred], name='vae_mlp')
 
-        inputs_x_value = tf.multiply(inputs_x, inputs_loss_weights)
-        outputs_x_value = tf.multiply(outputs_x, inputs_loss_weights)
-        reconstruction_loss = mse(inputs_x_value, outputs_x_value)
-        hx_log_var = tf.multiply(hx_log_var, inputs_loss_weights)
-        hx_mean = tf.multiply(hx_mean, inputs_loss_weights)
+        inputs_x = tf.multiply(inputs_x, inputs_loss_weights)
+        outputs_x = tf.multiply(outputs_x, inputs_loss_weights)
+        reconstruction_loss = mse(inputs_x, outputs_x)
+        # hx_log_var = tf.multiply(hx_log_var, inputs_loss_weights)
+        # hx_mean = tf.multiply(hx_mean, inputs_loss_weights)
         # reconstruction_loss = mse(inputs_x, outputs_x)
 
         noise = tf.math.subtract(inputs_x, outputs_x)
@@ -306,252 +307,6 @@ class DACVAE:
         kl_loss_z = -0.5 * K.sum(1 + hx_log_var - K.square(hx_mean) - K.exp(hx_log_var), axis=-1)
 
         pred_loss = K.categorical_crossentropy(inputs_batch, domain_pred)*self.input_size*self.domain_scale_factor
-        vae_loss = K.mean(reconstruction_loss + kl_loss_z*2 + pred_loss)
-
-        dann_vae.add_loss(vae_loss)
-        self.dann_vae = dann_vae
-        self.encoder = encoder_hx
-        self.decoder = decoder_x
-
-    def compile(self):
-        self.dann_vae.compile(optimizer=self.optimizer)
-        self.dann_vae.summary()
-
-    def train(self, x, batch, loss_weights, batch_size=100, epochs=300):
-
-        history = self.dann_vae.fit({'inputs': x, 'inputs_batch': batch, 'inputs_weights': loss_weights},
-                                        epochs=epochs, batch_size=batch_size,
-                                        validation_split=self.validation_split, shuffle=True)
-        return history
-
-    def get_output(self, x, batches,):
-        [z_mean, z_log_var, z] = self.encoder.predict([x, batches])
-        output_x = self.decoder.predict([z_mean, batches])
-        return z_mean, output_x
-
-
-class DAVAE2:
-    def __init__(self, input_size, latent_size=10, batches=2, path=''):
-        self.input_size = input_size
-        self.path = path
-        self.dann_vae = None
-        self.inputs = None
-        self.outputs_x = None
-        self.initializers = "glorot_uniform"
-        self.optimizer = optimizers.Adam(lr=0.01)
-        self.latent_z_size = latent_size
-        self.dropout_rate_small = 0.01
-        self.dropout_rate_big = 0.1
-        self.kernel_regularizer = regularizers.l1_l2(l1=0.00, l2=0.00)
-        self.validation_split = 0.0
-        self.batches = batches
-        self.dropout_rate = 0.01
-        callbacks = []
-        checkpointer = ModelCheckpoint(filepath=path + "vae_weights.h5", verbose=1, save_best_only=False,
-                                       save_weights_only=True)
-        reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.8, patience=10, min_lr=0.0001)
-        early_stop = EarlyStopping(monitor='loss', patience=60)
-        tensor_board = TensorBoard(log_dir=path + 'logs/')
-        callbacks.append(checkpointer)
-        callbacks.append(reduce_lr)
-        callbacks.append(early_stop)
-        callbacks.append(tensor_board)
-        self.callbacks = callbacks
-
-    def build(self):
-        Relu = "relu"
-        inputs_x = Input(shape=(self.input_size,), name='inputs')
-        inputs_batch = Input(shape=(self.batches,), name='inputs_batch')
-        inputs_loss_weights = Input(shape=(1,), name='inputs_weights')
-
-        hx = Dense(128, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
-                   name='hx_hidden_layer_x2')(inputs_x)
-        hx = BatchNormalization(center=True, scale=False)(hx)
-        hx = Activation(Relu)(hx)
-        hx = Dropout(self.dropout_rate_big)(hx)
-
-        hx = Dense(64, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
-                   name='hx_hidden_layer_x3')(hx)
-        hx = BatchNormalization(center=True, scale=False)(hx)
-        hx = Activation(Relu)(hx)
-        hx = Dropout(self.dropout_rate_small)(hx)
-
-        hx_mean = Dense(self.latent_z_size, kernel_regularizer=self.kernel_regularizer,
-                        kernel_initializer=self.initializers,
-                        name="hx_mean")(hx)
-        hx_log_var = Dense(self.latent_z_size, kernel_regularizer=self.kernel_regularizer,
-                           kernel_initializer=self.initializers,
-                           name="hx_log_var")(hx)
-        hx_z = Lambda(sampling, output_shape=(self.latent_z_size,), name='hx_z')([hx_mean, hx_log_var])
-        encoder_hx = Model(inputs_x, [hx_mean, hx_log_var, hx_z], name='encoder_hx')
-
-        latent_inputs_x = Input(shape=(self.latent_z_size,), name='latent')
-        x = Dense(64, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(latent_inputs_x)
-        x = BatchNormalization(center=True, scale=False)(x)
-        x = Activation(Relu)(x)
-        x = Dropout(self.dropout_rate_big)(x)
-        x = Dense(128, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(x)
-        x = BatchNormalization(center=True, scale=False)(x)
-        x = Activation(Relu)(x)
-        x = Dropout(self.dropout_rate_small)(x)
-        outputs_x = Dense(self.input_size, kernel_regularizer=self.kernel_regularizer,
-                          kernel_initializer=self.initializers, activation="softplus")(x)
-        decoder_x = Model(latent_inputs_x, outputs_x, name='decoder_x')
-
-        latent_inputs_batch = Input(shape=(self.latent_z_size,), name='latent_domain')
-        Flip = GradReverse()
-        d = Flip(latent_inputs_batch)
-        d = Dense(16, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(d)
-        d = BatchNormalization(center=True, scale=False)(d)
-        d = Activation(Relu)(d)
-        d = Dropout(0.05)(d)
-
-        d = Dense(self.batches, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
-                  activation="softmax")(d)
-        domian_classifier = Model(latent_inputs_batch, d, name='domain_classifier')
-
-        outputs_x = decoder_x(encoder_hx(inputs_x)[2])
-        domain_pred = domian_classifier(encoder_hx(inputs_x)[2])
-
-        dann_vae = Model([inputs_x, inputs_batch, inputs_loss_weights], [outputs_x, domain_pred], name='vae_mlp')
-
-        inputs_x_value = tf.multiply(inputs_x, inputs_loss_weights)
-        outputs_x_value = tf.multiply(outputs_x, inputs_loss_weights)
-        reconstruction_loss = mse(inputs_x_value, outputs_x_value)
-        # reconstruction_loss = mse(inputs_x, outputs_x)
-
-        noise = tf.math.subtract(inputs_x, outputs_x)
-        var = tf.math.reduce_variance(noise)
-        reconstruction_loss *= (0.5*self.input_size)/var
-        reconstruction_loss += (0.5*self.input_size)/var*tf.math.log(var)
-
-        kl_loss_z = -0.5 * K.sum(1 + hx_log_var - K.square(hx_mean) - K.exp(hx_log_var), axis=-1)
-
-        pred_loss = K.categorical_crossentropy(inputs_batch, domain_pred)*self.input_size*3
-        vae_loss = K.mean(reconstruction_loss + kl_loss_z*2 + pred_loss)
-
-        dann_vae.add_loss(vae_loss)
-        self.dann_vae = dann_vae
-        self.encoder = encoder_hx
-        self.decoder = decoder_x
-
-    def compile(self):
-        self.dann_vae.compile(optimizer=self.optimizer)
-        self.dann_vae.summary()
-
-    def train(self, x, batch, loss_weights, batch_size=100, epochs=300):
-        history = self.dann_vae.fit({'inputs': x, 'inputs_batch': batch, 'inputs_weights': loss_weights},
-                                    epochs=epochs, batch_size=batch_size,
-                                    validation_split=self.validation_split, shuffle=True)
-        return history
-
-    def get_output(self,x, batches,):
-        [z_mean, z_log_var, z] = self.encoder.predict(x)
-        output_x = self.decoder.predict(z_mean)
-        return output_x
-
-
-class DACVAE2:
-    def __init__(self, input_size, latent_size=10, batches=2, path=''):
-        self.input_size = input_size
-        self.path = path
-        self.dann_vae = None
-        self.inputs = None
-        self.outputs_x = None
-        self.initializers = "glorot_uniform"
-        self.optimizer = optimizers.Adam(lr=0.01)
-        self.latent_z_size = latent_size
-        self.dropout_rate_small = 0.01
-        self.dropout_rate_big = 0.1
-        self.kernel_regularizer = regularizers.l1_l2(l1=0.00, l2=0.00)
-        self.validation_split = 0.0
-        self.batches = batches
-        self.dropout_rate = 0.01
-        callbacks = []
-        checkpointer = ModelCheckpoint(filepath=path + "vae_weights.h5", verbose=1, save_best_only=False,
-                                       save_weights_only=True)
-        reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.8, patience=100, min_lr=0.0001)
-        early_stop = EarlyStopping(monitor='loss', patience=200)
-        tensor_board = TensorBoard(log_dir=path + 'logs/')
-        callbacks.append(checkpointer)
-        callbacks.append(reduce_lr)
-        callbacks.append(early_stop)
-        callbacks.append(tensor_board)
-        self.callbacks = callbacks
-
-    def build(self):
-        Relu = "relu"
-        inputs_x = Input(shape=(self.input_size,), name='inputs')
-        inputs_batch = Input(shape=(self.batches,), name='inputs_batch')
-        inputs_loss_weights = Input(shape=(1,), name='inputs_weights')
-        inputs = concatenate([inputs_x, inputs_batch])
-        hx = Dense(128, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
-                   name='hx_hidden_layer_x2')(inputs)
-        hx = BatchNormalization(center=True, scale=False)(hx)
-        hx = Activation(Relu)(hx)
-        hx = Dropout(self.dropout_rate_big)(hx)
-
-        hx = Dense(64, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
-                   name='hx_hidden_layer_x3')(hx)
-        hx = BatchNormalization(center=True, scale=False)(hx)
-        hx = Activation(Relu)(hx)
-        hx = Dropout(self.dropout_rate_small)(hx)
-
-        hx_mean = Dense(self.latent_z_size, kernel_regularizer=self.kernel_regularizer,
-                        kernel_initializer=self.initializers,
-                        name="hx_mean")(hx)
-        hx_log_var = Dense(self.latent_z_size, kernel_regularizer=self.kernel_regularizer,
-                           kernel_initializer=self.initializers,
-                           name="hx_log_var")(hx)
-        hx_z = Lambda(sampling, output_shape=(self.latent_z_size,), name='hx_z')([hx_mean, hx_log_var])
-        encoder_hx = Model([inputs_x, inputs_batch], [hx_mean, hx_log_var, hx_z], name='encoder_hx')
-
-        latent_inputs_x = Input(shape=(self.latent_z_size,), name='latent')
-        latent_inputs_batch = Input(shape=(self.batches,), name='latent_batch')
-        latent_inputs = concatenate([latent_inputs_x, latent_inputs_batch])
-        x = Dense(64, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(latent_inputs)
-        x = BatchNormalization(center=True, scale=False)(x)
-        x = Activation(Relu)(x)
-        x = Dropout(self.dropout_rate_big)(x)
-        x = Dense(128, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(x)
-        x = BatchNormalization(center=True, scale=False)(x)
-        x = Activation(Relu)(x)
-        x = Dropout(self.dropout_rate_small)(x)
-        outputs_x = Dense(self.input_size, kernel_regularizer=self.kernel_regularizer,
-                          kernel_initializer=self.initializers, activation="softplus")(x)
-        decoder_x = Model([latent_inputs_x, latent_inputs_batch], outputs_x, name='decoder_x')
-
-        latent_inputs_batch = Input(shape=(self.latent_z_size,), name='latent_domain')
-        Flip = GradReverse()
-        # d= grad_reverse(latent_inputs_batch)
-        d = Flip(latent_inputs_batch)
-        d = Dense(16, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(d)
-        d = BatchNormalization(center=True, scale=False)(d)
-        d = Activation(Relu)(d)
-        d = Dropout(0.05)(d)
-
-        d = Dense(self.batches, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
-                  activation="softmax")(d)
-        domian_classifier = Model(latent_inputs_batch, d, name='domain_classifier')
-
-        outputs_x = decoder_x([encoder_hx([inputs_x, inputs_batch])[2], inputs_batch])
-        domain_pred = domian_classifier(encoder_hx([inputs_x, inputs_batch])[2])
-
-        dann_vae = Model([inputs_x, inputs_batch, inputs_loss_weights], [outputs_x, domain_pred], name='vae_mlp')
-
-        inputs_x_value = tf.multiply(inputs_x, inputs_loss_weights)
-        outputs_x_value = tf.multiply(outputs_x, inputs_loss_weights)
-        reconstruction_loss = mse(inputs_x_value, outputs_x_value)
-        # reconstruction_loss = mse(inputs_x, outputs_x)
-
-        noise = tf.math.subtract(inputs_x, outputs_x)
-        var = tf.math.reduce_variance(noise)
-        reconstruction_loss *= (0.5*self.input_size)/var
-        reconstruction_loss += (0.5*self.input_size)/var*tf.math.log(var)
-
-        kl_loss_z = -0.5 * K.sum(1 + hx_log_var - K.square(hx_mean) - K.exp(hx_log_var), axis=-1)
-
-        pred_loss = K.categorical_crossentropy(inputs_batch, domain_pred)*self.input_size*3
         vae_loss = K.mean(reconstruction_loss + kl_loss_z + pred_loss)
 
         dann_vae.add_loss(vae_loss)
@@ -576,7 +331,9 @@ class DACVAE2:
         return z_mean, output_x
 
 
-def fit_integration(adata, batch_num=2, mode='DACVAE', split_by='batch', epochs=20, batch_size=128,
+
+
+def fit_integration(adata, batch_num=2, mode='DACVAE', split_by='batch_label', epochs=20, batch_size=128,
                     domain_lambda=1.0, sparse=True, hidden_layers=[128,64,32,5]):
     """/
     Build DAVAE model and fit the data to the model for training.
