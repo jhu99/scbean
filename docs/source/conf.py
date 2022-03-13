df163f42c1ce7bc9c0381321930cf028f978d144
drizzlezyk <43944770+drizzlezyk@users.noreply.github.com>
2022-03-13 20:42:46 +0800

    update tutorial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Jialu Hu'
 author = 'Jialu Hu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.3'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
