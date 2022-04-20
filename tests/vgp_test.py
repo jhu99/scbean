1ba8229c6c4d406280a7640e16b2a22953a4211f
wangyr997 <67814587+wangyr997@users.noreply.github.com>
2022-04-20 21:16:46 +0800

    Add files via upload

---
+++ tests/vgp_test.py
@@ -0,0 +1,22 @@
+import vgp
+import pandas as pd
+import multiprocessing as mp
+
+filepath = 'data/real_data/Rep11_MOB_count_matrix-1.tsv'
+data = pd.read_csv(filepath, sep='\t')
+# ---Split position---
+position = pd.DataFrame(index=data.index)
+position['x'] = data['Unnamed: 0'].str.split('x').str.get(0).map(float)
+position['y'] = data['Unnamed: 0'].str.split('x').str.get(1).map(float)
+data.drop('Unnamed: 0', axis=1, inplace=True)
+data = data.T[data.sum(0) >= 10].T  # Filter practically unobserved genes
+data = data.T  # genes * position
+position = position[data.sum(0) >= 10]
+data = data.T[data.sum(0) >= 10].T
+
+X = position.values
+Y = data
+
+if __name__ == '__main__':
+    result = vgp.run(X, Y, mp.cpu_count())
+    print(result)
