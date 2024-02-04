7367321931208aebfaff393dc341735247e5e3de
HaoHuiZhang <97244184+ZhangStudyHard@users.noreply.github.com>
2024-02-04 21:07:25 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../../'))
 
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-sys.path.insert(0, root_dir)
+# root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+# sys.path.insert(0, root_dir)
 
 # -- Project information -----------------------------------------------------
 
