7f97a27b086cd718d1e34d88a11d95f40175278c
HaoHuiZhang <97244184+ZhangStudyHard@users.noreply.github.com>
2024-02-05 15:40:01 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,11 +12,11 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('tools'))
+# sys.path.insert(0, os.path.abspath('tools'))
 # sys.path.insert(0, os.path.abspath("../../scbean"))
-#root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 # root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-#sys.path.insert(0, root_dir)
+sys.path.insert(0, root_dir)
 
 # -- Project information -----------------------------------------------------
 
