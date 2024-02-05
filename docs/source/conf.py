52ceb36f81050ac13dea5a77765b00da5721b02b
HaoHuiZhang <97244184+ZhangStudyHard@users.noreply.github.com>
2024-02-05 13:11:53 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,10 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('../..'))
-
-#root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-sys.path.insert(0, root_dir)
+sys.path.insert(0,os.path.absoath('../..'))
+# root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+# root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+# sys.path.insert(0, root_dir)
 
 # -- Project information -----------------------------------------------------
 
