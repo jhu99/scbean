60ac0d6444a1ca015c2dfdfc4c78fc3fde8fc479
HaoHuiZhang <97244184+ZhangStudyHard@users.noreply.github.com>
2024-02-05 13:44:22 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath("../../scbean"))
+#sys.path.insert(0, os.path.abspath("../../scbean"))
 # root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 # root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 # sys.path.insert(0, root_dir)
